d5df1028e002a15ec5f180c0d53c3d2d15ec804c
DeborahJoanny <>
2022-03-08 19:06:42 +0000

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,13 @@
+document.getElementById("enviar").addEventListener("click", validaFormulario)
+
+
+function validaFormulario(){
+
+  if(document.getElementById("nome-da-usuaria").value != "" &&
+      document.getElementById("email-da-usuaria").value != "") {
+
+    alert("Prontinho! Você receberá as novidades por email.")
+  } else {
+     alert("Por favor, preencha os campos nome e email.")
+  }
+}
